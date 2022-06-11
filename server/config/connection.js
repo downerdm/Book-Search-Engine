254201d235cc478c094acacac0332d64b73130c9
Dennis Downer <downerdm@gmail.com>
2022-06-11 09:29:16 -0400

    Deletion of node_modules

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/secret-shelf-71482', 
-  {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/secret-shelf-71482', 
+//   {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// module.exports = mongoose.connection;
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://downerdm:Password10@cluster0.7jspa.mongodb.net/secret-shelf-71482?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("secret-shelf-71482").collection("books");
+  // perform actions on the collection object
+  client.close();
 });
-
-module.exports = mongoose.connection;
